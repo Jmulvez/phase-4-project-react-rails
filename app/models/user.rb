32c011d9ea6a6aec3e95c10e7c0fbabe6b6bb5c6
John <johnmulvey48@yahoo.com>
2023-03-01 12:35:11 -0500

    added users and sessions controller actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :username, presence: true
+    validates :username, length: { in: 5..15 }
+    validates :username, uniqueness: true
+    validates :password_digest, presence: true
+    validates :profile_picture, presence: true
 end
