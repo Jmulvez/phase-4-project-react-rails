eb87529d157e521d95e6209cb41697148f9801bf
John <johnmulvey48@yahoo.com>
2023-03-02 11:46:24 -0500

    added relationships through models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :reviews
+    has_many :games, through: :reviews
     validates :username, presence: true
     validates :username, length: { in: 5..15 }
     validates :username, uniqueness: true
