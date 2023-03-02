053cfde16f5a0b33c9c0e7fd5d9dc821fef862a2
John <johnmulvey48@yahoo.com>
2023-03-02 13:30:05 -0500

    added tables for model layouts

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,10 @@
 class Game < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+    validates :title, presence: true
+    validates :title, uniqueness: true
+    validates :image_url, presence: true
+    validates :description, presence: true
+    validates :description, length: { in: 10..200 }
+    validates :developer, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
     belongs_to :user
     belongs_to :game
+    validates :comment, presence: true
+    validates :comment, length: { in: 4..100 }
+    validates :rating, presence: true
+    validates :rating, 
 end
