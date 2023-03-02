76c0a9dbbfb0d3087c91c30897316db89f4dc7a4
John <johnmulvey48@yahoo.com>
2023-03-02 13:44:18 -0500

    added model table attributes for migrations

--- db/migrate/20230302163800_create_games.rb
+++ db/migrate/20230302163800_create_games.rb
@@ -1,7 +1,10 @@
 class CreateGames < ActiveRecord::Migration[7.0]
   def change
     create_table :games do |t|
-
+      t.string :title
+      t.string :image_url
+      t.text :description
+      t.string :developer
       t.timestamps
     end
   end
--- db/migrate/20230302163814_create_reviews.rb
+++ db/migrate/20230302163814_create_reviews.rb
@@ -1,7 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
-
+      t.string :comment
+      t.integer :rating
+      t.integer :game_id
+      t.integer :user_id
       t.timestamps
     end
   end
