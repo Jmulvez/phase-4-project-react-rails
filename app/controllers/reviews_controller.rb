a4a1e3f3a8d23a018d900a57d8775296a5663eac
John <johnmulvey48@yahoo.com>
2023-03-03 13:33:40 -0500

    added review controller actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,44 @@
 class ReviewsController < ApplicationController
 
-    show 
+    def show 
+        review = Review.find(id: params[:id])
+        render json: review, status: :ok 
+    end 
 
-    index 
+    def index
+        reviews = Review.all 
+        render json: reviews, status: :ok
+    end
     
-    post 
+    def create
+        review = @current_user.reviews.create(review_params)
+        if review.valid?
+            render json: review, status: :created 
+        else
+            render json: { errors: ["Unprocessable Entity"] }, status: :unprocessable_entity
+        end
+    end
 
-    update
+    def update
+        review = @curent_user.reviews.find(id: params[:id])
+        review.update(review_params)
+        if review.valid?
+            render json: review, status: :accepted
+        else
+            render json: { errors: ["Unprocessable Entity"] }, status: :unprocessable_entity
+        end
+    end
 
-    delete
+    def delete 
+        review = @current_user.reviews.find(id: params[:id])
+        review.delete 
+        head :no_content
+    end
+
+    private 
+
+    def review_params 
+        params.permit(:comment, :rating)
+    end
 
 end
